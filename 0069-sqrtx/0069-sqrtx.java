3d14ed8150736e9e139c1e4be0c3575bd6455a88
Kavya Sharma <kavyasharma19.cse@gmail.com>
2026-03-28 17:07:17 +0530

    Time: 23 ms (6.46%), Space: 42.7 MB (34.04%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) 
+    {
+        if(x<2)
+        return x;
+        int i=2;
+        while((long)i*i<=x)
+        {
+            i++;
+        }
+        return i-1;
+    }
+}
