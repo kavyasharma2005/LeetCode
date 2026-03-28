7fe742b56f600a8e6b187cecdd8df54a4d289c44
Kavya Sharma <kavyasharma19.cse@gmail.com>
2026-03-28 17:18:53 +0530

    Time: 1 ms (83.33%), Space: 42.3 MB (42.62%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countOperations(int num1, int num2) 
+    {
+       int count=0;
+
+        while(num1!=0 && num2!=0 &&num1!=num2)
+        {
+            if(num1>num2)
+            {
+            num1=num1-num2;
+            count++;
+            }
+            else if(num2>num1)
+            {
+                num2=num2-num1;
+                count++;
+            }
+            
+        }
+        if(num1==num2 && num1==0)
+            return 0;
+        else if(num1==num2 && num1!=0)
+            return count+1;
+    return count;
+    }
+}
