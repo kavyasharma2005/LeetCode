895a6cfc3665b74bf1ec66ead1978b199f93b21f
Kavya Sharma <kavyasharma19.cse@gmail.com>
2026-03-28 17:23:04 +0530

    Time: 0 ms (100%), Space: 42.5 MB (40.11%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime)
+    {
+        int time=arrivalTime+delayedTime;
+        time=time%24;
+        return time;
+    }
+}
