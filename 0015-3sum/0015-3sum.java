0e86139f83e6cf63f1f23572063eaea520b2aa09
Kavya Sharma <kavyasharma19.cse@gmail.com>
2026-04-12 12:19:03 +0530

    Time: 35 ms (43.28%), Space: 59.2 MB (65.14%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,41 +1,43 @@
-import java.util.*;
-
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-
+    public List<List<Integer>> threeSum(int[] nums) 
+    {
+        List<List<Integer>> res =new ArrayList<>();
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) 
+        int sum=0,left,right;
+        for(int i=0;i<nums.length-2;i++)
         {
-            if (i > 0 && nums[i] == nums[i - 1]) 
+            
+            if(i>0 && nums[i]==nums[i-1])
             continue;
-            int left = i + 1;
-            int right = nums.length - 1;
 
-            while (left < right) 
+            left=i+1;
+            right=nums.length-1;
+
+            while(left<right)
             {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) 
-                {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+            sum=nums[i]+nums[left]+nums[right];
+            
+            if(sum==0)
+            {
+            res.add(Arrays.asList(nums[i],nums[left],nums[right]));
+            
+            left++;
+            right--;
+            while(left<right && nums[left-1]==nums[left])
+            left++;
 
-                    while (left < right && nums[left] == nums[left + 1]) left++;
+            while(left<right && nums[right]==nums[right+1])
+            right--;
+            
+            }
 
-                    while (left < right && nums[right] == nums[right - 1]) right--;
+            else if(sum<0)
+            left++;
+            else
+            right--;
 
-                    left++;
-                    right--;
-                }
-                else if (sum < 0) {
-                    left++;
-                }
-                else {
-                    right--;
-                }
             }
         }
-
-        return result;
+        return res;
     }
 }
