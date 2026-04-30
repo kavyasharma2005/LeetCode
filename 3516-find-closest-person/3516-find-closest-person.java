ba6413a4c6a96048e35c58256723d2a07dc21064
Kavya Sharma <kavyasharma19.cse@gmail.com>
2026-04-30 15:35:43 +0530

    Time: 1 ms (93.21%), Space: 42.5 MB (79.45%) - LeetHub

--- 3516-find-closest-person/3516-find-closest-person.java
+++ 3516-find-closest-person/3516-find-closest-person.java
@@ -1,6 +1,6 @@
 class Solution {
-    public int findClosest(int x, int y, int z)
-    { int dist1,dist2;
+    public int findClosest(int x, int y, int z) {
+       int dist1,dist2;
     dist1=z-x;
     dist2=z-y;
     if(Math.abs(dist1)<Math.abs(dist2))
@@ -8,6 +8,6 @@ class Solution {
     else if(Math.abs(dist1)>Math.abs(dist2))
     return 2;
     else 
-    return 0;
+    return 0; 
     }
 }
