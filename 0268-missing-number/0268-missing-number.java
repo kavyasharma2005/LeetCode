fffa1c13dd42982f7c22eebafadaad824c8192e6
Kavya Sharma <kavyasharma19.cse@gmail.com>
2026-06-28 23:34:32 +0530

    Time: 0 ms (100%), Space: 47.3 MB (48.13%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) 
     {
-        int sum=0,n=nums.length+1;
+        int sum=0,n=nums.length;
         for(int i=0;i<nums.length;i++)
         {
           sum+=nums[i];
         }
-        int misno=(((n-1)*(n))/2-sum);
+        int misno=(((n)*(n+1))/2-sum);
         return misno;
     }
 }
