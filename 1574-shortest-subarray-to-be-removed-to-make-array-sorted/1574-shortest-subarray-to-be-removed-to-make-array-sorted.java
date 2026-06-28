71b3c127b939ca853eb38a9c767ddd6a295bcbd0
Kavya Sharma <kavyasharma19.cse@gmail.com>
2026-06-28 15:48:01 +0530

    Time: 1 ms (99.85%), Space: 85.6 MB (18.59%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.java
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.java
@@ -1,39 +1,39 @@
-import java.util.*;
-
 class Solution {
+
     public int findLengthOfShortestSubarray(int[] arr) {
 
         int n = arr.length;
 
         int left = 0;
+
         while (left + 1 < n && arr[left] <= arr[left + 1]) {
             left++;
         }
 
-        if (left == n - 1) return 0;
+        if (left == n - 1)
+            return 0;
 
         int right = n - 1;
+
         while (right > 0 && arr[right - 1] <= arr[right]) {
             right--;
         }
 
         int ans = Math.min(n - left - 1, right);
 
-        // Just storing suffix indices in a deque
-        Deque<Integer> dq = new ArrayDeque<>();
+        int i = 0;
+        int j = right;
 
-        for (int i = right; i < n; i++) {
-            dq.offerLast(i);
-        }
+        while (i <= left && j < n) {
 
-        for (int i = 0; i <= left; i++) {
+            if (arr[i] <= arr[j]) {
 
-            while (!dq.isEmpty() && arr[dq.peekFirst()] < arr[i]) {
-                dq.pollFirst();
-            }
+                ans = Math.min(ans, j - i - 1);
+                i++;
 
-            if (!dq.isEmpty()) {
-                ans = Math.min(ans, dq.peekFirst() - i - 1);
+            } else {
+
+                j++;
             }
         }
 
