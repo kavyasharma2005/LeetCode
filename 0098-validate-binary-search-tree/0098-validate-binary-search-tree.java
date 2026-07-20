867fb4024f10e84cce70de661d826a84e0489bf5
Kavya Sharma <kavyasharma19.cse@gmail.com>
2026-07-20 20:23:00 +0530

    Time: 0 ms (100%), Space: 45.1 MB (65.81%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,25 +13,23 @@
  *     }
  * }
  */
-class Solution 
-{
-
-    TreeNode prev = null;
+class Solution {
 
     public boolean isValidBST(TreeNode root) 
+    {
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean helper(TreeNode root, long min, long max) 
     {
 
         if (root == null)
             return true;
 
-        if (!isValidBST(root.left))
+        if (root.val <= min || root.val >= max)
             return false;
 
-        if (prev != null && root.val <= prev.val)
-            return false;
-
-        prev = root;
-
-        return isValidBST(root.right);
+        return helper(root.left, min, root.val) &&
+               helper(root.right, root.val, max);
     }
 }
