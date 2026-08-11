06cef5ec8658d46298ab2fe7575c5483d88d66cc
Kavya Sharma <kavyasharma19.cse@gmail.com>
2026-08-11 21:06:38 +0530

    Time: 1 ms (93.72%), Space: 43.8 MB (92.05%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int missingInteger(int[] nums) {
+        int sum = nums[0];
+
+        // Find the sum of the longest sequential prefix
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            } else {
+                break;
+            }
+        }
+
+        // Check if sum exists in the array
+        while (true) {
+            boolean found = false;
+
+            for (int num : nums) {
+                if (num == sum) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                return sum;
+            }
+
+            sum++;
+        }
+    }
+}
