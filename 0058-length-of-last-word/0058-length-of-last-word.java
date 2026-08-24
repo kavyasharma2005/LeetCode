3b966b75fbf24cf274d045b647b054ec17d83f03
Kavya Sharma <kavyasharma19.cse@gmail.com>
2026-08-24 22:21:24 +0530

    Time: 2 ms (9.3%), Space: 43.3 MB (16.2%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,11 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s=s.trim();
-        for(int i=s.length()-1;i>=0;i--)
-        {
-            if(s.charAt(i)==' ')
-            return (s.length()-i-1);
-        }
-     return s.length();
+        String[] words = s.trim().split("\\s+");
+        return words[words.length-1].length();
     }
 }
